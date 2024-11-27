69971d044c07333e2c1e6aaf31d070aa69893910
Adarsha <jivraj@gamil.com>
2024-11-27 13:12:58 +0545

    small sign up work

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -6,23 +6,30 @@ const Login=()=>{
 
   return (
     
-    <div className="container-fluid overflow-hidden  bg-light m-5 p-5">
+    <div style={{
+      backgroundColor: '#FFDAB9'}}className="container-fluid overflow-hidden m-5 p-5">
       <Form>
        <div className="text-start mx-2">
         <p className=" text-muted mb-1"style={{ fontWeight: 500 }}>Please enter your details</p>
        <p className="fs-1 fw-bold">Welcome back</p>
        </div>
         <Col className=" d-grid gap-4 m-4 mt-5">
-          <Form.Group as={Row} >
+          <Form.Group as={Row}style={{
+              border: '1px solid #ccc',
+              borderRadius: '4px',
+            }} >
           <Form.Control type="email"placeholder="email"></Form.Control>
           </Form.Group>
-          <Form.Group as={Row} >
+          <Form.Group as={Row}style={{
+              border: '1px solid #ccc',
+              borderRadius: '4px',
+            }} >
           <Form.Control type="password" placeholder="password"></Form.Control>
           </Form.Group>
         </Col>
         <Row >
         <Form.Group as={Col} className="mx-2" >
-        <Form.Check type="checkbox" style={{ fontWeight: 500 }}label=" Remember for 30 days"></Form.Check>
+        <Form.Check type="checkbox"  style={{ fontWeight: 500 }}label=" Remember for 30 days"></Form.Check>
         </Form.Group>
         <Form.Group as={Col} xs="auto" className="mx-3" >
        <Link to="/forgetPassword"> Forgot password</Link> 
